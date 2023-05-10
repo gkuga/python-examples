07458008c6d7ec06943374a24b787b9cd063b341
gkuga <gaku.yasui@gmail.com>
2023-05-11 03:41:59 +0900

    Add main4

---
+++ concurrent/main4.py
@@ -0,0 +1,48 @@
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
+
+def func(i):
+    sleep(1)
+    print(f"{i}\n", flush=True, end="")
+
+
+print('通常')
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for i in range(5):
+        executor.submit(
+            func, i
+        )
+
+
+print('迷走＆失敗')  # https://qiita.com/ninomiyt/items/b2546806847e199a3a9b
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for i in range(5):
+        executor.submit(
+            lambda: func(i)
+        )
+
+
+print('迷走2')
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for i in range(5):
+        executor.submit(
+            lambda num: func(num), i
+        )
+
+
+print('迷走3')
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for i in range(5):
+        executor.submit(
+            lambda num=i: func(num)
+        )
+
+
+print('迷走4')
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for i in range(5):
+        executor.submit(
+            partial(func, i)
+        )
