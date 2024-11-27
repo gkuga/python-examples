ec8ae992e55681fa21688cf8cbba391939787db7
Gaku Yasui <gaku.yasui@gmail.com>
2024-11-27 17:25:59 +0900

    Add scikit-learn

---
+++ scikit-learn/main.py
@@ -0,0 +1,18 @@
+# データ加工・処理・分析ライブラリ
+import sklearn
+import numpy as np
+import numpy.random as random
+import scipy as sp
+from pandas import Series, DataFrame
+import pandas as pd
+
+# 可視化ライブラリ
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+%matplotlib inline
+
+# 機械学習ライブラリ
+
+# 小数第3位まで表示
+%precision 3
